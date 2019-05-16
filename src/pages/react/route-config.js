54abcc603f28b270611c1abd99d8034e1d9241c6
Michael Liendo <model3volution@gmail.com>
2019-05-16 12:35:35 -0500

    add display names to routes for table of contents

--- src/pages/react/route-config.js
+++ src/pages/react/route-config.js
@@ -10,38 +10,35 @@ import moduleChallenge from './markdown/module-challenge.md'
 
 export default [
   {
+    displayName: 'Overview',
     path: '/overview',
     markdown: overviewMarkdown,
   },
   {
+    displayName: 'Why?',
     path: '/why',
     markdown: whyMarkdown,
   },
+  { displayName: 'Setup', path: '/setup', markdown: setupMarkdown },
   {
-    path: '/setup',
-    markdown: setupMarkdown,
-  },
-  {
+    displayName: 'Intro to Components',
     path: '/components-intro',
     markdown: componentsIntroMarkdown,
   },
   {
+    displayName: 'Composition',
     path: '/composition',
     markdown: compositionMarkdown,
   },
+  { displayName: 'Props', path: '/props', markdown: propsMarkdown },
+  { displayName: 'State', path: '/state', markdown: stateMarkdown },
   {
-    path: '/props',
-    markdown: propsMarkdown,
-  },
-  {
-    path: '/state',
-    markdown: stateMarkdown,
-  },
-  {
+    displayName: 'Project: Gif Loader',
     path: '/gif-loader-functional',
     markdown: gifLoaderFunctional,
   },
   {
+    displayName: 'Module Challenge',
     path: '/module-challenge',
     markdown: moduleChallenge,
   },
