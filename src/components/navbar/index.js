7605862de0f350482382eb2979191728119ffaa1
Michael Liendo <mtliendo@gmail.com>
2019-06-26 09:17:48 -0500

    put signout content in modal

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactModal from 'react-modal'
 
 import { Link } from 'react-router-dom'
 import './navbar.scss'
 import { FEDTrainingLogo } from './FED-Logo'
 
 function Navbar({ user, handleSignOut }) {
+  const [isModalOpen, setModal] = useState(false)
   const username = user && user.username
+
+  const toggleModal = () => {
+    setModal(!isModalOpen)
+  }
+
   return (
     <nav className="navigation">
-      <Link
-        to="/"
-        className="nav-primary nav-item training-logo"
-      >
+      <Link to="/" className="nav-primary nav-item training-logo">
         <FEDTrainingLogo />
       </Link>
       <div className="nav-secondary">
+        {username && (
+          <span
+            className={'nav-item'}
+            onClick={() => {
+              toggleModal()
+            }}
+          >
+            Welcome{`, ${username}`}
+          </span>
+        )}
+      </div>
+      <ReactModal isOpen={isModalOpen}>
         <span className={'nav-item'} onClick={handleSignOut}>
           Sign Out
         </span>
-        {username && (
-          <span className={'nav-item'}>Welcome{`, ${username}`}</span>
-        )}
-      </div>
+      </ReactModal>
     </nav>
   )
 }
