a2929e134161b33c931f6b44f4fb1667674a68a7
Michael Liendo <model3volution@gmail.com>
2019-12-12 09:53:49 -0600

    add link to content videos

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -9,6 +9,14 @@ function Navbar() {
       <Link to="/" className="nav-primary nav-item training-logo">
         <FEDTrainingLogo />
       </Link>
+      <a
+        className="nav-item nav-secondary"
+        href="https://drive.google.com/drive/folders/1YW3KoU4HiSWgmLl8VTJr478NjeaV87-G"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Supplemental Videos
+      </a>
     </nav>
   )
 }
